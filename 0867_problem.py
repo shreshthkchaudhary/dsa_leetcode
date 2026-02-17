894479d726df0e99fc564e42907d84c243bd937e
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-02-17 22:42:32 +0530

    uploding new logic

--- 0867_problem.py
+++ 0867_problem.py
@@ -1,12 +1,26 @@
 # 867. Transpose Matrix
 
 def transpose(matrix):
-    res=[]
-    for i in range (len(matrix[0])):
-        temp=[]
-        for j in range (len(matrix)):
-            temp.append(matrix[j][i])
-        res.append(temp)
-    return res
+    # res=[]
+    # for i in range (len(matrix[0])):
+    #     temp=[]
+    #     for j in range (len(matrix)):
+    #         temp.append(matrix[j][i])
+    #     res.append(temp)
+    # return res
+
+    ans =[[0]*len(matrix) for _ in range(len(matrix[0]))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            ans[j][i]= matrix[i][j]
+    return ans
+
+    # # in case of square matrix
+    # for i in range (0,len(matrix)-1):
+    #     for j in range (i,len(matrix)):
+    #         matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+    # return matrix
+
 
 print(transpose([[1,2,3],[4,5,6],[7,8,9]]))
--- 1351_problem.py
+++ 1351_problem.py
@@ -9,13 +9,26 @@ def countNegatives(grid):
     # return count
 
 
+    # count=0
+    # for i in range(len(grid)):
+    #     for j in range(len(grid[i])-1,-1,-1):
+    #         if grid[i][j] < 0:
+    #             count+=1
+    #         elif grid[i][j] >= 0:
+    #             break
+    # return count
+
+    n=len(grid)
+    m=len(grid[0])
+    i=0
+    j=m-1
     count=0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])-1,-1,-1):
-            if grid[i][j] < 0:
-                count+=1
-            elif grid[i][j] >= 0:
-                break
+    while i<n and j>=0:
+        if grid[i][j]<0:
+            count=count+(n-i)
+            j=j-1
+        else:
+            i=i+1
     return count
 
 
